bac5d747e75dc7eee17ceff836774b073fe64567
mestauder <94409804+mestauder@users.noreply.github.com>
2022-05-02 22:31:05 -0400

    Add files via upload

--- RentaCar.java
+++ RentaCar.java
@@ -9,47 +9,17 @@ package rentacar;
  * @author mattstauder
  */
 public class RentaCar {
-    //Variables
-    private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
-    private javax.swing.JLabel jLabel9;
-    private javax.swing.JLabel jLabel10;
     
-    private java.awt.List CarList;
-    private java.awt.List Customerlist;
-    private java.awt.List Branchlist;
-    private java.awt.List Reservationlist;
-    
-    
-    
-    public GUI() {
-        insertComponents();
-    }
-    
-    private void insertComponents() {
-        jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        User users = new User();
+        
+        
+        
+        LoginPage loginPage = new LoginPage(users.getInfo());
 }
+}    
+
+//*URL of home button icon: https://www.flaticon.com/free-icon/home-button_60817
